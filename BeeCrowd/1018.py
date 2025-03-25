80db74793461373a498e18ea2c748c1cd60edd0c
Ezeki3l-sys <alemaomeira197@gmail.com>
2025-03-24 21:02:46 -0300

    dfs

--- BeeCrowd/1018.py
+++ BeeCrowd/1018.py
@@ -1,11 +1,20 @@
-from math import floor
+# -*- coding: utf-8 -*-
 
 numero = int(input())
 
-cem = floor(numero/100)
-cinquenta = floor((numero%100)/50)
-vinte = floor((numero%50)/20)
-dez = floor((numero%20)/10)
-cinco = floor((numero%10)/5)
-um = floor(numero%5)
-print(f"{cem} nota(s) de 100\n{cinquenta} nota(s) de 50\n{vinte} nota(s) de 20\n{dez} nota(s) de 10\n{cinco} nota(s) de 5\n{um} moeda(s) de 1")
+cem = numero//100
+numero = numero-cem*100
+cinquenta = numero//50
+numero = numero-cinquenta*50
+vinte = numero//20
+numero = numero-vinte*20
+dez = numero//10
+numero = numero-dez*10
+cinco = numero//5
+numero = numero-cinco*5
+dois = numero//2
+numero = numero-dois*2
+um = numero//1
+numero = numero-um
+
+print(f"{cem} nota(s) de R$ 100,00\n{cinquenta} nota(s) de R$ 50,00\n{vinte} nota(s) de R$ 20,00\n{dez} nota(s) de R$ 10,00\n{cinco} nota(s) de R$ 5,00\n{dois} nota(s) de R$ 2,00\n{um} nota(s) de R$ 1,00")
--- BeeCrowd/1035.py
+++ BeeCrowd/1035.py
@@ -1,9 +1,7 @@
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
+numeros = input().split()
+print(numeros)
 
-if(b>c and d>a and c+d>a+b and c>=0 and d>=0 and a%2==0):
-    print("Valores aceitos")
-else:
-    print("Valores nao aceitos")
+# if(b>c and d>a and c+d>a+b and c>=0 and d>=0 and a%2==0):
+#     print("Valores aceitos")
+# else:
+#     print("Valores nao aceitos")
