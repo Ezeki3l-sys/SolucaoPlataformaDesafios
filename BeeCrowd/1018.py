015dfe645dac7f00bc2a47697ae282e84e16dca0
Ezeki3l-sys <alemaomeira197@gmail.com>
2025-04-10 18:53:01 -0300

    asd

--- BeeCrowd/1018.py
+++ BeeCrowd/1018.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 numero = int(input())
-
+numero2 = numero
 cem = numero//100
 numero = numero-cem*100
 cinquenta = numero//50
@@ -16,5 +16,5 @@ dois = numero//2
 numero = numero-dois*2
 um = numero//1
 numero = numero-um
-
+print(numero2)
 print(f"{cem} nota(s) de R$ 100,00\n{cinquenta} nota(s) de R$ 50,00\n{vinte} nota(s) de R$ 20,00\n{dez} nota(s) de R$ 10,00\n{cinco} nota(s) de R$ 5,00\n{dois} nota(s) de R$ 2,00\n{um} nota(s) de R$ 1,00")
